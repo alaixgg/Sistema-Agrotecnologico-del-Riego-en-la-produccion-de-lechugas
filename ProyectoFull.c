9ad6870e1463cd9930569ae2c4cb10e8740a1eb0
Andres Alaix <80136164+alaixgg@users.noreply.github.com>
2024-05-06 21:52:22 -0500

    Funcioal bomba de agua
    Calibrar sensor de humedad, revisar que ocurre con la resistencia de luminusidad
    Arreglar envio a gmail

--- ProyectoFull.c
+++ ProyectoFull.c
@@ -1,3 +1,4 @@
+
 // Según el valor arrojado por el sensor:
 // 0 ~ 300: suelo seco
 // 300 ~ 700: suelo húmedo
@@ -6,25 +7,42 @@
 int segmentos[7] = {23, 22, 21, 19, 18, 19, 5};
 const int pinH = 26;
 int valorSensorHumedad = 0; // sensor de humedad
+const int pinBombaAgua = 12; // Pin de control de la bomba de agua
+int LDR = 14;
+int luminusidad;
 
 void setup()
 {
-    Serial.begin(115200);
+  Serial.begin(115200);
+  // Configurar el pin de la bomba de agua como salida
+  pinMode(pinBombaAgua, OUTPUT);
 
+  //Configurar el pin de la luminusidad como salida
+  pinMode(LDR, INPUT);
 }
 
 void loop()
 {
+  //humedad
     valorSensorHumedad = analogRead(pinH);
-    Serial.print("Valor del sensor:  ");
+    Serial.print("Valor del sensor de humedad:  ");
     Serial.println(valorSensorHumedad);
-    //delay(1000);
+    
+  // luminusidad
+    luminusidad = analogRead(LDR);
+    Serial.println("Valor del sensor:  ");
+    Serial.println(luminusidad);
+
 
     if (valorSensorHumedad < 300)
     {
         Serial.print("La lechuga necesita agua ");
         Serial.print(valorSensorHumedad);
         Serial.println(" Riegue hasta llegar a un valor aproximado de 400-600");
+        digitalWrite(pinBombaAgua, HIGH); // Activar la bomba de agua
+        Serial.println("Activando bomba de agua...");
+        delay(2000);
+        digitalWrite(pinBombaAgua, LOW); // Desactivar la bomba de agua
       
     
     }
